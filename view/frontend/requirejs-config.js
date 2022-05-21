5bddd83229e451f6afa70ca24bc1f4d8f16def86
John <john@getjohn.co.uk>
2022-05-21 11:55:26 +0100

    GJDIB-1909 show checkout agreements block for Direct method

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     "map": {
         "*": {
-            "Paymentsense_Payments/template/payment/method/hosted/form.html": "GetJohn_PaymentsensePaymentsOverride/template/payment/method/hosted/form.html"
+            "Paymentsense_Payments/template/payment/method/hosted/form.html": "GetJohn_PaymentsensePaymentsOverride/template/payment/method/hosted/form.html",
+            "Paymentsense_Payments/template/payment/method/direct/form.html": "GetJohn_PaymentsensePaymentsOverride/template/payment/method/direct/form.html"
         }
     }
 };
